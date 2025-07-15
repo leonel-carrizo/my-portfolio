3bd2fdedc93de70edb399b6007e508239f4daa3e
leonel <102891741+leonel-carrizo@users.noreply.github.com>
2025-07-15 23:12:31 +0100

    fix: resposive greeting secction

--- src/components/Layouts/Greeting/Greeting.jsx
+++ src/components/Layouts/Greeting/Greeting.jsx
@@ -7,9 +7,8 @@ function Greeting ({ name }) {
   return (
     <section className='greeting-section'>
       <div className='greeting-section__content'>
-        <p> Hi I'm </p>
-        <h1 className='section-name'>{name}</h1>
-        <p>Software developer</p>
+        <p> Hi I'm <span className='section-name'>{name}</span>.</p>
+        <p>Software developer.</p>
       </div>
       <div className='section-img'>
         <img className='profile_img' src='../../../../src/assets/profile2.png' alt='Leonel Profile Picture' />
