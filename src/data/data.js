5282d3340fd4df3a282b6968eb869b11a8a3e08c
leonel <102891741+leonel-carrizo@users.noreply.github.com>
2025-07-27 02:07:20 +0100

    add file data to pass it as a prop dynamicaly

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+const profileData = {
+  name: 'Leonel Carrizo',
+  nickName: 'Leo',
+  title: 'Software Developer',
+  contactMail: 'hello@leonelcarrizo.com',
+  githubUrl: 'https://www.github.com/leonel-carrizo',
+  imgProfile: 'src/assets/profile2.webp'
+}
+
+export default profileData
